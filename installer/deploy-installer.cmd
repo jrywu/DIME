c056cf99fcfa591ecdde28c02a30e3d09403df84
Jeremy Wu <jrywjwu@gmail.com>
2013-08-23 12:41:03 +0800

    update deployment script

--- installer/deploy-installer.cmd
+++ installer/deploy-installer.cmd
@@ -3,10 +3,10 @@ mkdir "..\Installer\system32.x86"
 mkdir "..\Installer\system32.x64
 copy ..\Release\Win32\TSFTTS.dll ..\Installer\system32.x86\
 copy ..\Release\x64\TSFTTS.dll ..\Installer\system32.x64\
+"c:\Program Files (x86)\NSIS\makensis.exe" TSFTTS-x8664.nsi
+REM use the following line instead for buiding on x86 platform
+REM "c:\Program Files\NSIS\makensis.exe" TSFTTS-x8664.nsi
 echo ===============================================================================
-echo Please download and install Nullosft Sciptable Install System (NSIS) comiler . 
-echo                                  ---http://nsis.sourceforge.net/Download-----
-echo Then compile TSFDayi-x86.nsi and TSFDayi-x64.nsi to build installer packages for 
-echo windows 32bit and 64 bit or TSFDayi-x8664.nsi for integrated 32/64bit installer.
+echo Deployment and Installer packaging done!!
 echo ===============================================================================
 pause
