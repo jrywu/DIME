81c7172f54e6b77d85b2f71e4656d2fc6d7228c8
Jeremy Wu <jrywjwu@gmail.com>
2022-02-28 19:13:15 +0800

    Update build info to ver 1.2

--- buildInfo.cmd
+++ buildInfo.cmd
@@ -5,9 +5,9 @@ del commitcount.txt
 set BUILD_YEAR_1=%DATE:~2,2%
 rem set /a _CommitCount+=1
 set _BuildDate1=%BUILD_YEAR_1%%DATE:~5,2%%DATE:~8,2%
-set _BuildVersion=1.1.%_CommitCount%.%_BuildDate1%
+set _BuildVersion=1.2.%_CommitCount%.%_BuildDate1%
 echo #define BUILD_VER_MAJOR 1 >BuildInfo.h
-echo #define BUILD_VER_MINOR 1 >>BuildInfo.h
+echo #define BUILD_VER_MINOR 2 >>BuildInfo.h
 echo #define BUILD_COMMIT_COUNT %_CommitCount% >>BuildInfo.h
 echo #define BUILD_YEAR_4 %DATE:~0,4% >>BuildInfo.h
 echo #define BUILD_YEAR_2 %DATE:~2,2% >>BuildInfo.h
